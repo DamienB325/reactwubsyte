1a89bd58b0ee96bb2e4f7e8f27ec182cd4b06b27
DamienB325 <damien.brown15@outlook.com>
2023-05-15 09:43:38 -0700

    Add a quote from dumpcan

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          The wubsytes under construction.
+          "The website's under construction." - Duncan
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Powered by React
         </a>
       </header>
     </div>
